10e6e33a81f64365203b51bb04381d28862b45c6
hoga60 <71911755+hoga60@users.noreply.github.com>
2023-05-29 09:20:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,27 @@
-from flask import Flask
+from flask import Flask, request
+from linebot import LineBotApi, WebhookHandler
+from linebot.models import TextSendMessage   # 載入 TextSendMessage 模組
+import json
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route("/", methods=['POST'])
+def linebot():
+    body = request.get_data(as_text=True)
+    json_data = json.loads(body)
+    print(json_data)
+    try:
+        line_bot_api = LineBotApi('你的 Channel access token')
+        handler = WebhookHandler('你的 LINE Channel secret')
+        signature = request.headers['X-Line-Signature']
+        handler.handle(body, signature)
+        tk = json_data['events'][0]['replyToken']         # 取得 reply token
+        msg = json_data['events'][0]['message']['text']   # 取得使用者發送的訊息
+        text_message = TextSendMessage(text=msg)          # 設定回傳同樣的訊息
+        line_bot_api.reply_message(tk,text_message)       # 回傳訊息
+    except:
+        print('error')
+    return 'OK'
+
+if __name__ == "__main__":
+    app.run()
